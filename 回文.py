5b59ab41810ca01b24885088218456c415e4648a
梁君牧 <575706085@qq.com>
2025-11-06 00:11:47 +0800

    删除

--- 回文.py
+++
@@ -1,6 +0,0 @@
-#回文数
-num = input("请输入一个数字:")
-if num == num[::-1]:
-    print("是回文数")
-else:
-    print("不是回文数")
--- 水仙花数.py
+++
@@ -1,6 +0,0 @@
-# 水仙花数
-def is_narcissistic_number(num):
-    sum = 0
-    for i in range(len(str(num))):
-        sum += int(str(num)[i])**3
-    return sum == num
